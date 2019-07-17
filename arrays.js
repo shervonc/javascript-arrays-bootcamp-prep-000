96a3f1e5519a9bae493760a95db6bf781f18348c
Von C <shervonc@gmail.com>
2019-07-17 13:41:56 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,41 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+
+function addElementToBeginningofArray(array, element) {
+ return [element,...array];
+}
+
+function destructivelyAddElementToBeginnngofArray(array, element) {
+   
+  return array.unshift(element)
+  
+}
+
+function addElementToEndofArray(array, element) {
+ return [...array, element];
+}
+
+function destructivelyAddElementToEndofArray(array, element){
+    array.push(element)
+    return array;
+}
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+  array.shift()
+  return array;
+  }
+  
+  function removeElementFromBeginningOfArray(array) {
+   return array.slice(1);
+  }
+  function destructivelyRemoveElementFromEndOfArray(array) {
+    array.pop()
+    return array;
+  }
+  function removeElementFromEndOfArray(array) {
+    return array.slice(0,array.length -1);
+  }
+  
+  
